3f868dab699ee005497c4807530e3217a5a2b377
Alviannn <alvianndq@gmail.com>
2022-02-24 19:33:16 +0700

    Temporarily added default values for testing

--- src/com/github/alviannn/padieshop/Main.java
+++ src/com/github/alviannn/padieshop/Main.java
@@ -2,7 +2,10 @@ package com.github.alviannn.padieshop;
 
 import com.github.alviannn.padieshop.menus.HomeMenu;
 import com.github.alviannn.padieshop.menus.UserMenu;
+import com.github.alviannn.padieshop.models.products.ClothProduct;
+import com.github.alviannn.padieshop.models.products.FoodProduct;
 import com.github.alviannn.padieshop.models.products.Product;
+import com.github.alviannn.padieshop.models.products.TechnologyProduct;
 import com.github.alviannn.padieshop.models.users.User;
 
 import java.util.ArrayList;
@@ -22,10 +25,25 @@ public class Main {
     public User CURRENT_USER = null;
     public final User ADMIN;
 
+    /**
+     * Filling default values to the app for testing purposes.
+     */
+    private void fillWithDefault() {
+        USER_LIST.add(new User("alvian", "Alvian DQ", "alvianndq@gmail.com", "hello123"));
+
+        PRODUCT_LIST.add(new FoodProduct("Milo", 8_000, "12 Des 2022"));
+        PRODUCT_LIST.add(new ClothProduct("Jaket Anime", 130_000, "XL"));
+        PRODUCT_LIST.add(new TechnologyProduct("Logitech", 520_000, "G231 Prodigy"));
+
+        CURRENT_USER = USER_LIST.get(0);
+    }
+
     public Main() {
         this.USER_LIST = new ArrayList<>();
         this.PRODUCT_LIST = new ArrayList<>();
         this.ADMIN = new User("admin", "", "", "admin123");
+
+        this.fillWithDefault();
 
         HomeMenu homeMenu = new HomeMenu(this);
         UserMenu userMenu = new UserMenu(this);
