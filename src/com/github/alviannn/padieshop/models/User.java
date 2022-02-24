a7379f8b6c655c6c64e330d3d856318abd215f68
Alviannn <alvianndq@gmail.com>
2022-02-24 21:20:24 +0700

    Removed redundant imports

--- src/com/github/alviannn/padieshop/models/User.java
+++ src/com/github/alviannn/padieshop/models/User.java
@@ -1,6 +1,5 @@
 package com.github.alviannn.padieshop.models;
 
-import com.github.alviannn.padieshop.models.Receipt;
 import com.github.alviannn.padieshop.models.products.Product;
 
 import java.util.ArrayList;
